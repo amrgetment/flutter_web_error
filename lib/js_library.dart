5b5968f72ffd55fcda8e2141c570022e55e0b59c
Amr Eniou <aeniou@getment.io>
2023-12-19 13:52:57 +0200

    init

--- lib/js_library.dart
+++ lib/js_library.dart
@@ -3,7 +3,6 @@ library script.js;
 
 import 'dart:js_interop';
 
-
 // This function will do Promise to return something
 @JS()
 external String jsPromiseFunction(String message);
@@ -11,5 +10,6 @@ external String jsPromiseFunction(String message);
 // This function will open new popup window for given URL.
 @JS()
 external String jsOpenTabFunction(String url);
+
 ///         ^            ^                ^
 ///      return     functionName       arguments
